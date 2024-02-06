54e51255a2c1bfc768bc41d91313f7f52b9a092e
Rafael <54147384+RafaRpso@users.noreply.github.com>
2024-02-06 12:53:21 -0300

    fix(): cheat

--- VersãoFinal/bossrush gamejam/Assets/LeaderBoard/LeaderboardManager.cs
+++ VersãoFinal/bossrush gamejam/Assets/LeaderBoard/LeaderboardManager.cs
@@ -12,12 +12,12 @@ namespace LeaderboardCreatorDemo
         [SerializeField] public TMP_Text[] _entryTextObjects;
         [SerializeField] public TMP_InputField _usernameInputField;
 
-// Make changes to this section according to how you're storing the player's score:
-// ------------------------------------------------------------
+        // Make changes to this section according to how you're storing the player's score:
+        // ------------------------------------------------------------
         [SerializeField] public GameController _exampleGame;
-        
+
         public int Score => (int)_exampleGame.statisticSettings.timeToCompleteGame;
-// ------------------------------------------------------------
+        // ------------------------------------------------------------
 
         private void Start()
         {
@@ -29,19 +29,19 @@ namespace LeaderboardCreatorDemo
         {
             // Q: How do I reference my own leaderboard?
             // A: Leaderboards.<NameOfTheLeaderboard>
-        
+
             Leaderboards.NoMoreAngels.GetEntries(entries =>
             {
-                if(_entryTextObjects[0] == null)
+                if (_entryTextObjects[0] == null)
                 {
                     _entryTextObjects = new TMP_Text[entries.Length + 5];
                 }
 
-                for(int i = 0; i < _entryTextObjects.Length; i++)
+                for (int i = 0; i < _entryTextObjects.Length; i++)
                 {
                     GameObject gObj = Instantiate(texto, new Vector2(this.transform.position.x, this.transform.position.y - i * 45), transform.rotation);
                     gObj.transform.SetParent(gameObject.transform);
-                    if( _entryTextObjects[i] != null)
+                    if (_entryTextObjects[i] != null)
                     {
                         Destroy(_entryTextObjects[i].gameObject);
                     }
@@ -57,19 +57,23 @@ namespace LeaderboardCreatorDemo
                     var minutes = (int)totalMinutes;
                     var seconds = (int)((totalMinutes - minutes) * 60);
                     //_entryTextObjects[i] = Instantiate(texto, new Vector2(this.transform.position.x, this.transform.position.y - i*5), transform.rotation).GetComponent<TMP_Text>();
-                    _entryTextObjects[i].text = $"{entries[i].Rank}. {entries[i].Username} - {minutes:D2}:{seconds:D2}";
+                    _entryTextObjects[i].text = $"{entries[i].Rank}. {entries[i].Username} - {minutes:D2}:{seconds:D2} min ";
                     //_entryTextObjects.Length ++;
 
-            }});
+                }
+            });
         }
-        
+
         public void UploadEntry()
         {
-            Leaderboards.NoMoreAngels.UploadNewEntry(_usernameInputField.text, Score, isSuccessful =>
+            if (!_exampleGame.playerSettings.firstTimeCheat)
             {
-                if (isSuccessful)
-                    LoadEntries();
-            });
+                Leaderboards.NoMoreAngels.UploadNewEntry(_usernameInputField.text, Score, isSuccessful =>
+                {
+                    if (isSuccessful)
+                        LoadEntries();
+                });
+            }
         }
     }
 }
